33a6cb7212b959abbbfcd4fb352988ceac0e317a
slchangtw <shunlung.chang@gmail.com>
2024-05-23 20:59:20 +0200

    Add source code

---
+++ src/process.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import yaml
+from sklearn.model_selection import train_test_split
+
+BINARY_COLUMNS = [
+    "mainroad",
+    "guestroom",
+    "basement",
+    "hotwaterheating",
+    "airconditioning",
+    "prefarea",
+]
+
+CATEGORY_COLUMNS = [
+    "furnishingstatus",
+]
+
+
+def process_housing(housing: pd.DataFrame) -> pd.DataFrame:
+    housing = housing.copy()
+
+    # Binary encoding
+    for col in BINARY_COLUMNS:
+        housing[col] = housing[col].apply(lambda x: 1 if x == "yes" else 0)
+
+    # One-hot encoding
+    housing = pd.get_dummies(housing, columns=CATEGORY_COLUMNS)
+
+    return housing
+
+
+def split_data(housing: pd.DataFrame, test_size: float, seed: int) -> pd.DataFrame:
+    train, test = train_test_split(housing, test_size=test_size, random_state=seed)
+
+    return train, test
+
+
+if __name__ == "__main__":
+    with open("params.yaml", "r") as f:
+        params = yaml.safe_load(f)
+
+    housing = pd.read_csv(params["data_source_path"])
+
+    housing_processed = process_housing(housing)
+    housing_train, housing_test = split_data(
+        housing_processed, params["test_size"], params["seed"]
+    )
+
+    housing_train.to_csv(params["data_train_path"], index=False)
+    housing_test.to_csv(params["data_test_path"], index=False)
---
+++ src/train.py
@@ -0,0 +1,33 @@
+import pickle
+
+import pandas as pd
+import yaml
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import MinMaxScaler
+
+
+def train(housing_train: pd.DataFrame, n_estimators: int) -> None:
+    X_train = housing_train.drop("price", axis=1)
+    y_train = housing_train["price"]
+
+    model = Pipeline(
+        [
+            ("scaler", MinMaxScaler()),
+            ("gbr", GradientBoostingRegressor(n_estimators=n_estimators)),
+        ]
+    )
+    model.fit(X_train, y_train)
+
+    return model
+
+
+if __name__ == "__main__":
+    with open("params.yaml", "r") as f:
+        params = yaml.safe_load(f)
+
+    housing_train = pd.read_csv(params["data_train_path"])
+    model = train(housing_train, params["n_estimators"])
+
+    with open(params["model_path"], "wb") as f:
+        pickle.dump(model, f)
